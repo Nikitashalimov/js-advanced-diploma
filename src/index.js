53b63392eb0497ec79e59dfc3d64f77018325432
Nikita Shalimov <nikitashalimov@mail.ru>
2021-12-04 14:20:05 +0300

    вроде запускается

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './js/app';
 
 // entry point for webpack
 // don't write your code here
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};

65125a1e5bf51638233acce296320537b8a6ca36
Nikita Shalimov <nikitashalimov@mail.ru>
2021-12-11 18:09:15 +0300

    4 шаг

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,24 @@
 export function calcTileType(index, boardSize) {
   // TODO: write logic here
-  return 'center';
+  if (index === 0) {
+    return 'top-left';
+  } else if (index === 7) {
+    return 'top-right';
+  } else if (index === 56) {
+    return 'bottom-left';
+  } else if (index === 63) {
+    return 'bottom-right';
+  } else if (index >= 1 && index <= 6) {
+    return 'top';
+  } else if (index >= 57 && index <= 62) {
+    return 'bottom';
+  } else if (index % 8 === 0) {
+    return 'left';
+  } else if (index % 8 === 7) {
+    return 'right';
+  } else {
+    return 'center';
+  }
 }
 
 export function calcHealthLevel(health) {
